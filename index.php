a34d9a46421b8832100722de3452f27346915db3
changal123 <70043114+changal123@users.noreply.github.com>
2021-02-13 14:06:19 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+	<!-- Bootstrap core CSS -->
+	<link href="style/bootstrap.min.css" rel="stylesheet">
+
+	<title>Ace in the Hole Multisport Events</title>
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+	<link href="style/style.less" rel="stylesheet/less">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+	<div id="masthead">
+
+		<header class="wrapper">
+
+			<?php include 'includes/nav.html.php'; ?>
+
+		</header>
+	</div>
+
+
+	<main class="wrapper">
+
+		<?php include 'includes/body.html.php'; ?>
+
+	</main>
+
+	<div id="footer-outer">
+
+		<?php include 'footer.html.php'; ?>
+
+	</div>
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="scripts/bootstrap.min.js"></script>
+
+</body>
+
+</html>
