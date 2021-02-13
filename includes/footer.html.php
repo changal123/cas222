d5e7c796e8653a278774a01d9aa8117036642be7
changal123 <70043114+changal123@users.noreply.github.com>
2021-02-13 14:07:14 -0800

    Add files via upload

---
+++ includes/footer.html.php
@@ -0,0 +1,25 @@
+
+		<footer class="wrapper">
+
+			<div id="social-icons">
+				<p>
+					<a href="https://facebook.com" target="_blank">
+						<i class="fab fa-facebook-square"></i>
+					</a>
+
+					<a href="https://twitter.com" target="_blank">
+						<i class="fab fa-twitter-square"></i>
+					</a>
+
+					<a href="https://youtube.com" target="_blank">
+						<i class="fab fa-youtube-square"></i>
+					</a>
+
+					<a href="https://instagram.com" target="_blank">
+						<i class="fab fa-instagram"></i>
+					</a>
+				</p>
+			</div>
+			<p>Blah Blah &copy; 2021</p>
+
+		</footer>
