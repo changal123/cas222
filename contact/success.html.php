00cc4de8ef417d6dace11d35179d075a0465c301
changal123 <70043114+changal123@users.noreply.github.com>
2021-02-25 20:18:24 -0800

    Add files via upload

---
+++ contact/success.html.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Ace in the Hole Multisport Events</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet/less" href=../style/style.less>
+        <link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div id="wrapper">
+            <?php include '../includes/header.html.php'; ?>
+            <?php include '../includes/nav.html.php'; ?>
+            <main>
+                <h1>Success!</h1>
+                <p>A cheerful member of our staff will be in contact with you soon:<br>
+                  Name: <?php echo htmlspecialchars($myName, ENT_QUOTES, 'UTF-8'); ?><br>
+                  Email: <?php echo htmlspecialchars($myEmail, ENT_QUOTES, 'UTF-8'); ?><br>
+                  Question: <?php echo htmlspecialchars($myQuestion, ENT_QUOTES, 'UTF-8'); ?><br>
+                </p>
+            </main>
+            <?php include '../includes/footer.html.php'; ?>
+        </div>
+    </body>
+</html>
