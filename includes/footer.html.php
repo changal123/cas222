b182eb15f41e028fff1c8fb69b83b2081f2577c3
changal123 <70043114+changal123@users.noreply.github.com>
2021-03-16 22:12:41 -0700

    Add files via upload

---
+++ includes/footer.html.php
@@ -0,0 +1,25 @@
+
+		<footer class="wrapper">
+
+			<div id="social-icons">
+				<p>
+					<a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" target="_blank">
+						<i class="fab fa-facebook-square"></i>
+					</a>
+
+					<a href="https://twitter.com/pcccas222?lang=en" target="_blank">
+						<i class="fab fa-twitter-square"></i>
+					</a>
+
+					<a href="https://youtube.com" target="_blank">
+						<i class="fab fa-youtube-square"></i>
+					</a>
+
+					<a href="https://instagram.com" target="_blank">
+						<i class="fab fa-instagram"></i>
+					</a>
+				</p>
+			</div>
+			<p>Blah Blah &copy; 2021</p>
+
+		</footer>
